2e1328b36fba34bb76defec453df5e89cb9a148b
Rafael-C-C <xxrafaflooxx@gmail.com>
2021-11-22 11:01:27 -0600

    Funciones

---
+++ Final/KaninosTest/LoginTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using kaninos;
+using Kaninos.Controllers;
+using kaninos.Entities;
+using kaninos.Models;
+using kaninos.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+namespace KaninosTest
+{
+    [TestClass]
+    public class LoginTest
+    {
+        static string connectionString = "Data Source=LAPTOP-AP83LF2M;Initial Catalog=kaninos;Persist Security Info=False;User ID=kaninos_user;Password=12345";
+        static DbContextOptions<ApplicationDbContext> options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(connectionString).Options;
+        ApplicationDbContext dbContext = new ApplicationDbContext(options);
+
+        public Microsoft.AspNetCore.Hosting.IWebHostEnvironment HostingEnvironment { get; }
+
+        [TestMethod]
+        public void formatoLoginvacio_Compruebaqueellogin_notedejepasarconloscamposvacíos()
+        {
+            //arrange
+            var dto = new LoginDTO
+            {
+                email = null,
+                pass = null,
+            };
+            var expected = false;
+
+            //act
+            EjemplaresController controller = new EjemplaresController(dbContext, HostingEnvironment);
+            var result = controller.formatoLoginvacio(dto);
+
+            //assert
+            Assert.AreEqual(expected, result);
+        }
--- Final/kaninos/Controllers/HomeController.cs
+++ Final/kaninos/Controllers/HomeController.cs
@@ -8,23 +8,22 @@ using Microsoft.Extensions.Logging;
 using kaninos.Models;
 using kaninos.Data;
 using kaninos.Entities;
-
-
+using System.Text.RegularExpressions;
 
 namespace kaninos.Controllers
 {
 
-    
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _dbContext;
-        public HomeController(ILogger<HomeController> logger,ApplicationDbContext dbContext)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
         {
             _logger = logger;
             _dbContext = dbContext;
         }
-   
+
 
         public IActionResult Index()
         {
@@ -50,11 +49,11 @@ namespace kaninos.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Login(LoginDTO dto)
         {
-            var login =_dbContext.Logins.FirstOrDefault(login => login.email == dto.email && login.pass == dto.pass);            
-            
-            ViewBag.Message="Email y Contraseña No Coinciden";
-            
-            return login == null ? View() : RedirectToAction("Administrador","Home");
+            var login = _dbContext.Logins.FirstOrDefault(login => login.email == dto.email && login.pass == dto.pass);
+
+            ViewBag.Message = "Email y Contraseña No Coinciden";
+
+            return login == null ? View() : RedirectToAction("Administrador", "Home");
         }
 
         public IActionResult Registro()
@@ -81,7 +80,7 @@ namespace kaninos.Controllers
 
             return RedirectToAction("Login");
         }
-        
+
         public IActionResult Noticias()
         {
             return View();
@@ -91,13 +90,102 @@ namespace kaninos.Controllers
         {
             return View();
         }
-        
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        
+
+        #region Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult New(LoginDTO dto)
+        {
+            var email = _dbContext.Logins.Select(email => new LoginDTO
+            {
+                email = email.email,
+            });
+
+            var pass = _dbContext.Logins.Select(pass => new LoginDTO
+            {
+                pass = pass.pass,
+            });
+
+            ViewBag.email = email;
+            ViewBag.pass = pass;
+
+
+            if (email(dto) == null)
+            {
+                ViewBag.Message0 = "Por favor, ingrese un correo valido";
+            }
+
+            if (email(dto) == null)
+            {
+                ViewBag.Message1 = "Por favor, ingrese una contraseña valida";
+            }
+            return View();
+
+        }
+        #endregion
+
+        #region funciones
+        //Login
+        public bool emailvacio(LoginDTO dto)
+        {
+            bool result = false;
+
+            try
+            {
+                if (Regex.IsMatch(dto.email, @"^[a-zA-Z\u00f1\u00d1\u00E0-\u00FC\s]+$"))
+                {
+                    result = true;
+                }
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public Login Emailexist(LoginDTO dto)
+        {
+            try
+            {
+                var email = _dbContext.Logins.FirstOrDefault(x => x.email == dto.email);
+                return email;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+
+        public bool CorrectPass(LoginDTO dto)
+        {
+            bool result = false;
+
+            try
+            {
+                if (Emailexist(dto).pass==dto.pass)
+                {
+                    result = true;
+                }
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+        //finlogin
+        #endregion
+      
     }
 }
