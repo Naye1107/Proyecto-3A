115e294bfdd8b6109acaa3d4548e3dca65bf7ac4
Naye1107 <91940768+Naye1107@users.noreply.github.com>
2021-11-26 00:20:39 -0600

    Update LoginDTO.cs

--- Final/kaninos/Models/LoginDTO.cs
+++ Final/kaninos/Models/LoginDTO.cs
@@ -1,13 +1,19 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace kaninos.Models
 {
     public class LoginDTO
     {
+        [Required]
         public int id_log { get; set; }
+        [Required]
         public string nombre { get; set; }
+        [Required]
         public string apellido { get; set; }
+        [Required]
         public string email { get; set; }
+        [Required]
         public string pass { get; set; }
     }
 }
